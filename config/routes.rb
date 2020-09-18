f088c611d70262dab621a24c389a0b4567927900
ctdupuis <ctdupuis7@gmail.com>
2020-09-17 19:29:41 -0500

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   get "/", to: "users#login"
+  get '/logout', to: 'users#logout'
+  get '/logged_in', to: 'users#logged_in'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

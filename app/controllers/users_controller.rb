593c535493c52358e238bbe7f264c46734005c40
ctdupuis <ctdupuis7@gmail.com>
2020-09-17 19:01:46 -0500

    basic login functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,30 @@
 class UsersController < ApplicationController
   def login
-    # binding.pry
-    render json: { "test": "wop" }
+      user = User.find_by(username: params[:username])
+      if user && user.authenticate(params[:password])
+          user_reqs = Request.filter(user)
+          session[:user_id] = user.id
+          render json: { 
+              logged_in: true,
+              user: user.as_json(only: [:id, :username, :f_name, :l_name, :admin]) }
+      else user
+          render json: { logged_in: false, error: "Invalid username/password combination"}
+      end
+  end
+
+  def logged_in
+      if @current_user
+          render json: { 
+              logged_in: true,
+              user: @current_user
+          }
+      else
+          render json: { logged_in: false }
+      end
+  end
+
+  def logout
+      session.clear
+      render json: { logged_in: false }
   end
 end

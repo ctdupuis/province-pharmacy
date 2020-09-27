c10ba030f8927bce5b063513167552f6902f218f
ctdupuis <ctdupuis7@gmail.com>
2020-09-27 12:20:33 -0500

    added check for new password, update action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,25 +2,30 @@ class UsersController < ApplicationController
     before_action :set_user, only: [:login, :update]
 
   def login
-    #   user = User.find_by(username: params[:username].upcase)
-      if params[:password] == ENV['PASS']
-        # binding.pry
+      # user = User.find_by(username: params[:username].upcase)
+      if params[:password] == ENV['PASS'] && @user.authenticate(params[:password])
+        binding.pry
         render json: { 
             first_login: true,
             user: @user.as_json(only: [:id, :username, :admin])
         }
-      elsif user && user.authenticate(params[:password])
-        #   session[:user_id] = user.id
+      elsif @user && @user.authenticate(params[:password])
+          # session[:user_id] = @user.id
           render json: { 
               logged_in: true,
               user: @user.as_json(only: [:id, :username, :admin]) 
-            }
-      else user
+          }
+      else
           render json: { logged_in: false, error: "Invalid username/password combination"}
       end
   end
 
   def update
+    # @user.update_attribute(:password, params[:password])
+    render json: { 
+              logged_in: true,
+              user: @user.as_json(only: [:id, :username, :admin]) 
+            }
     binding.pry
   end
 

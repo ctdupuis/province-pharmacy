1e02e9d48ede467ad3abfeb5f180dbdfc19eb236
ctdupuis <ctdupuis7@gmail.com>
2021-01-04 22:54:19 -0600

    separate Conversation/Message demo models

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,18 +1,22 @@
 class MessagesController < ApplicationController
     
     def create
-        message = Message.new(message_params)
-        conversation = Conversation.find(message_params[:conversation_id])
+        if params[:user][:demo]
+            conversation = Conversation.find(2)
+        else
+            conversation = Conversation.find(1)
+        end
+        message = Message.new(
+            text: params[:text],
+            user_id: params[:user][:id],
+            conversation_id: conversation.id
+        )
+        # binding.pry
         if message.save
-            serialized_data = MessageSerializer.new(message).to_serialized_json
+            serialized_data = MessagesSerializer.new(message).to_serialized_json
             MessagesChannel.broadcast_to conversation, serialized_data
             head :ok
         end
     end
 
-    private
-
-    def message_params
-        params.require(:message).permit(:text, :conversation_id)
-    end
 end

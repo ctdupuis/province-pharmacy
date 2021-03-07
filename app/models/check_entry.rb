b1d85a86649c9014b62ef5869dd27c7a451cb3e8
ctdupuis <ctdupuis7@gmail.com>
2021-03-07 12:57:07 -0600

    created method for CheckEntry

--- app/models/check_entry.rb
+++ app/models/check_entry.rb
@@ -7,6 +7,16 @@ class CheckEntry < ApplicationRecord
         return self.original_amt - self.adjusted_amt
     end
 
+    def created
+        self.created_at.in_time_zone('Central Time (US & Canada)').to_date
+    end
+
+    def as_json(options={})
+        super(except: [:created_at, :updated_at], 
+            methods: [:created, :price_difference]
+        )
+    end
+
     private
 
     def format_rx_num
--- app/models/delivery_entry.rb
+++ app/models/delivery_entry.rb
@@ -6,7 +6,6 @@ class DeliveryEntry < ApplicationRecord
     validates :patient_address, presence: true
 
     def created
-        # self.created_at.in_time_zone('Central Time (US & Canada)').strftime("%m/%d/Y")
         self.created_at.in_time_zone('Central Time (US & Canada)').to_date
     end
 

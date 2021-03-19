a74dbd67ec75ac16a14ff096d1fed467a266a28e
ctdupuis <ctdupuis7@gmail.com>
2021-03-18 20:12:21 -0500

    configure session_store.rb

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,5 @@
 if Rails.env == "production"
-    Rails.application.config.session_store :cookie_store, key: "_province_pharmacy", domain: "province-client.herokuapp.com"
+    Rails.application.config.session_store :cookie_store, key: "_province_pharmacy", domain: "province-client.herokuapp.com", same_site: :none, secure: true
 else   
     Rails.application.config.session_store :cookie_store, key: "_province_pharmacy"
 end

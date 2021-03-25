0a4fee6da21fbf31cd6ee54f08223fb567773bc0
ctdupuis <ctdupuis7@gmail.com>
2021-03-24 19:09:06 -0500

    adjust action_cable.url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -40,7 +40,7 @@ Rails.application.configure do
 
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
-  config.action_cable.url = 'wss://province-portal.herokuapp.com/cable'
+  config.action_cable.url = 'wss://province-pharmacy.herokuapp.com/cable'
   config.action_cable.allowed_request_origins = [ 'https://province-portal.herokuapp.com', /http:\/\/province-portal.herokuapp.com.*/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.

32bd57b1c26b30cffa6c1f0d3e3a17e56007ad15
ctdupuis <ctdupuis7@gmail.com>
2021-03-28 19:30:39 -0500

    copy over methods from delivery_entry, validations

--- app/models/route.rb
+++ app/models/route.rb
@@ -1,4 +1,16 @@
 class Route < ApplicationRecord
     belongs_to :delivery_log
     belongs_to :user
+
+    def date_format
+        self.created_at.in_time_zone('Central Time (US & Canada)').strftime("%m/%d/%y")
+    end
+
+    def as_json(options={})
+        super(include: {
+            user: {
+                only: [:username, :first_name]
+            }
+        }, methods: [:date_format])
+    end
 end
--- app/models/stop.rb
+++ app/models/stop.rb
@@ -1,3 +1,6 @@
 class Stop < ApplicationRecord
     belongs_to :route
+
+    validates :patient_name, presence: true
+    validates :patient_address, presence: true
 end
